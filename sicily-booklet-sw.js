5e57a3456feafd37a2c8a77d9d3fc8be3f0b2654
antonior56-only <antonior56@gmail.com>
2026-09-01 16:10:12 +0200

    Add files via upload

--- sicily-booklet-sw.js
+++ sicily-booklet-sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "sicily-booklet-v3";
-const CORE_FILES = ["./index.html", "sicily-booklet.webmanifest", "sicily-icon.svg", "sicily-icon-192.png", "sicily-icon-512.png"];
+const CACHE_NAME = "sicily-booklet-v4";
+const CORE_FILES = ["./", "./index.html", "./sicily-booklet.webmanifest", "./sicily-icon.svg", "./sicily-icon-192.png", "./sicily-icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_FILES)).then(() => self.skipWaiting()));
@@ -12,5 +12,10 @@ self.addEventListener("message", event => {
 });
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+  const isPageRequest = event.request.mode === "navigate" || new URL(event.request.url).pathname.endsWith(".html");
+  if (isPageRequest) {
+    event.respondWith(fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html") || caches.match("./"))));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match("./index.html"))));
 });
